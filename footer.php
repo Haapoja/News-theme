b5fa9e625b7a312b4bc29b9f085c4bf18cfdd051
saysaii <krisu.97@hotmail.com>
2020-09-24 08:12:23 +0300

    added privacy page template and a footer link for it

--- footer.php
+++ footer.php
@@ -1,31 +1,32 @@
 <?php wp_footer();?>
 <footer class="page-footer font-small blue pt-4">
 
-  <!-- Footer Links -->
+  
   <div class="container-fluid text-center text-md-left">
 
-    <!-- Grid row -->
+  
     <div class="row">
 
-      <!-- Grid column -->
+     
       <div class="col-md-6 mt-md-0 mt-3">
 
-        <!-- Content -->
+      
         <h5 class="text-uppercase">Contact us</h5>
         <p>Email.Email@com</p>
         <p>01010101010101</p>
         <p>Generic Address</p>
+        <a href="<?php echo site_url("/privacy-policy") ?>">Privacy Policy</a>
         
 
       </div>
-      <!-- Grid column -->
+  
 
       <hr class="clearfix w-100 d-md-none pb-3">
 
-      <!-- Grid column -->
+     
       <div class="col-md-3 mb-md-0 mb-3">
 
-        <!-- Links -->
+      
         <h5 class="text-uppercase">Categories</h5>
 
         <ul class="list-unstyled">
@@ -47,12 +48,12 @@
         </ul>
 
       </div>
-      <!-- Grid column -->
+  
 
-      <!-- Grid column -->
+     
       <div class="col-md-3 mb-md-0 mb-3">
 
-        <!-- Links -->
+     
         <h5 class="text-uppercase">Social Media</h5>
 
         <ul class="list-unstyled">
@@ -71,13 +72,13 @@
         </ul>
 
       </div>
-      <!-- Grid column -->
+      
 
     </div>
-    <!-- Grid row -->
+   
 
   </div>
-  <!-- Footer Links -->
+ 
 
 
 
---
+++ privacy.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+<?php /* Template Name: Privacy */ ?>
+<div class="container">
+<?php echo the_content()?>
+</div>
+
+<?php
+ get_footer();?>
